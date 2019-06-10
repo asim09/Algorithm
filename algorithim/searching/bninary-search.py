4020446c9315ee636c95b0223432e0da9e6ca622
asim khan <measim8@gmail.com>
2019-06-10 10:02:45 +0530

    minor changes in program

--- algorithim/searching/bninary-search.py
+++ algorithim/searching/bninary-search.py
@@ -1,3 +1,33 @@
+# Method -1
+
+def Binary_search(arr, start_index, last_index, element):
+    while start_index <= last_index:
+        mid = (int)(start_index + last_index) // 2
+
+        if element > arr[mid]:
+            start_index = mid + 1
+        elif element < arr[mid]:
+            last_index = mid - 1
+        elif element == arr[mid]:
+            return mid
+    return -1
+
+
+arr = [2, 3, 4, 10, 40,45,80]
+element = 40
+start_index = 0
+last_index = len(arr) - 1
+found = Binary_search(arr, start_index, last_index, element)
+if found == -1:
+    print("element not present in array")
+
+else:
+    print("element is present at index " + str(found))
+
+
+
+# Method -1 Recusrion
+
 
 def binary_search(sample_array, number, start, end):
     if start < end:
