7d33d01d104fca7905d185d94457fc948fd4d64e
asim09 <khan.asim87@gmail.com>
2023-11-12 15:07:07 +0530

    code change

--- problems.py
+++ problems.py
@@ -1,14 +1,11 @@
 
+number_list = [1,2,3]
 
 
+for n, i in enumerate(number_list):
+    print()
+    if i > 50:
+        print(f"Greater Numbers: {i} with index {n}, new list-{[len(number_list)]}-| {number_list}")
+        number_list.remove(i)
+        print(f"Removed Greater Numbers- {i}, iteration: {n} new list-{[len(number_list)]}-| {number_list}")
 
-
-# for n, i in enumerate(number_list):
-#     print()
-#     if i > 50:
-#         print(f"Greater Numbers: {i} with index {n}, new list-{[len(number_list)]}-| {number_list}")
-#         number_list.remove(i)
-#         print(f"Removed Greater Numbers- {i}, iteration: {n} new list-{[len(number_list)]}-| {number_list}")
-
-# i = 0
-# while i < len(number_list):
