07d6bbd97acc5ec3367cfc63aced94ba07f1cb25
asim09 <khan.asim87@gmail.com>
2023-11-13 12:11:25 +0530

    code change

---
+++ run.py
@@ -0,0 +1,63 @@
+def findLongestSubstring(string):
+    n = len(string)
+    st = 0
+    maxlen = 0
+    start = 0
+    pos = {}
+    pos[string[0]] = 0
+
+    for i in range(1, n):
+        if string[i] not in pos:
+
+            pos[string[i]] = i
+            # print(f'={pos}=={[string[i]]}===={i}')
+            # print()
+        else:
+            # print(f'={pos}=={[string[i]]}===={i}')
+            if pos[string[i]] >= st:
+                currlen = i - st
+                if maxlen < currlen:
+                    maxlen = currlen
+                    start = st
+                st = pos[string[i]] + 1
+            # print(f'==={pos}')
+            # print(f'==={pos[string[i]]}==={i}')
+            pos[string[i]] = i
+            # print(f'={pos}=={[string[i]]}===={i}')
+    # if maxlen < i - st:
+    #     print('==================================')
+    #     maxlen = i - st
+    #     start = st
+    # print(start,start + maxlen)
+    return string[start: start + maxlen]
+
+
+def solve(str: str) -> int:
+    if len(str) == 0:
+        return 0
+    maxans = -1
+    for i in range(len(str)):
+        set = {}
+        for j in range(i, len(str)):
+            print(f"i-->{i}, j--->{j}, set----{set}, maxans---- {maxans}")
+            if str[j] in set:
+                maxans = max(maxans, j - i)
+                break
+            set[str[j]] = 1
+    return maxans
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+    # string = "GEEKSFORGEEKS"
+    # string = "abcabcbb"
+    # print(findLongestSubstring(string))
